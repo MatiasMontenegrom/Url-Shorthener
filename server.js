01ba3ddfb12635d8fc532dd96810c0e7fc54b579
MatiasMontenegrom <matiasmontenegrom@gmail.com>
2022-08-09 16:14:29 -0300

    commit;

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/shortUrls',async (req, res) => {
     res.redirect('/');
 })
 
-app.get('/:shortUrl', async (req, res) => {
+app.get('https://url-shorthener.herokuapp.com//:shortUrl', async (req, res) => {
     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
 
     if(shortUrl == null) return res.sendStatus(404);
